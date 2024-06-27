eaa90adc70bf54b0e5fabf322d80d3c0319e681b
bravos2k5 <nationalbao2005@gmail.com>
2024-06-27 16:07:39 +0700

    update-utils

--- src/main/java/utils/XSecurity.java
+++ src/main/java/utils/XSecurity.java
@@ -1,5 +1,15 @@
 package utils;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 public class XSecurity {
+
+    public static String hashPassword(String originPassword) {
+        return BCrypt.hashpw(originPassword,BCrypt.gensalt());
+    }
+
+    public static boolean isValidPassword(String password, String hashedPassword) {
+        return BCrypt.checkpw(password,hashedPassword);
+    }
 
 }
