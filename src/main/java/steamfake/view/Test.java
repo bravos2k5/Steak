b91733338bac2d313b7b8c0a42647658fce5617d
bravos2k5 <nationalbao2005@gmail.com>
2024-07-06 00:05:28 +0700

    update-radius-label

---
+++ src/main/java/steamfake/view/Test.java
@@ -0,0 +1,50 @@
+/*
+ * Created by JFormDesigner on Fri Jul 05 22:25:21 ICT 2024
+ */
+
+package steamfake.view;
+
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+
+import javax.swing.*;
+import steamfake.graphics.*;
+
+/**
+ * @author BRAVOS
+ */
+public class Test extends JFrame {
+    public Test() {
+        initComponents();
+    }
+
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
+        UIManager.setLookAndFeel(new FlatMacDarkLaf());
+        new Test().setVisible(true);
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        // Generated using JFormDesigner Evaluation license - baonqps41272
+
+        //======== this ========
+        var contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 398, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .addGap(0, 269, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - baonqps41272
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
