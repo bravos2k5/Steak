9895b97b7045e6f53015c5e26eb031fdcc2c4cae
bravos2k5 <nationalbao2005@gmail.com>
2025-01-10 22:18:25 +0700

    add file

--- src/main/java/steamfake/Main.java
+++ src/main/java/steamfake/Main.java
@@ -6,8 +6,10 @@ import steamfake.view.mainframe.MFrame;
 import javax.swing.*;
 
 public class Main {
+
     public static void main(String[] args) throws UnsupportedLookAndFeelException {
         UIManager.setLookAndFeel(new FlatMacDarkLaf());
         SwingUtilities.invokeLater(MFrame::gI);
     }
+
 }
